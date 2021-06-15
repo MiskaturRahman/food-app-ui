97fec614dfe401e6ff1d214e4c4cf15266a338c1
Miskatur Rahman <rahmnmisk007@gmail.com>
2021-06-15 22:21:26 +0600

    first commit

---
+++ components/Home.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text } from "react-native";
+import Feather from "react-native-vector-icons/Feather";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import catagoriesData from "../assets/data/categoriesData";
+import popularData from "../assets/data/popularData";
+
+function Home() {
+  return <Text>Home</Text>;
+}
+
+export default Home;
